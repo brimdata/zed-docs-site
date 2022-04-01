5f1c97f38bdb1fc054f95e1249b7ce28351e0080
Matthew Nibecker <hello@mattnibecker.com>
2022-04-01 11:54:35 -0400

    zed.brimdata.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Zed",
   tagline: "Working with data has never been easier.",
-  url: "https://brimdata.github.io",
-  baseUrl: "/zed-docs-site/",
+  url: "https://zed.brimdata.io",
+  baseUrl: "/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
